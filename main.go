6c1b03cdc0b4dbf9a41c8f7767d219eead8ae751
yujc <yujc@youshu.cc>
2019-06-30 18:01:19 +0800

    更新go

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"time"
 
 	pb "grpc-sample/Pb_Go"
 	"golang.org/x/net/context"
@@ -10,7 +12,7 @@ import (
 )
 
 const (
-	port = ":50051"
+	addr = ":50051"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -18,14 +20,17 @@ type server struct{}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, u *pb.User) (*pb.Response, error) {
-	return &pb.Response{ErrCode: 0, ErrMsg: "", Data: map[string]string{"name": "Hello " + u.Name}}, nil
+	return &pb.Response{ErrCode: 0, ErrMsg: "success", Data: map[string]string{"name": "Hello " + u.Name}}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
+	fmt.Printf("%s server start at %s\n", time.Now(), addr)
+
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
 	s.Serve(lis)
